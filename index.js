bee228b1381e45402e4db26dca47c90a10213d17
Charles Mambo <chylah11st@gmail.com>
2023-07-06 22:29:20 +0200

    Complete project

---
+++ index.js
@@ -0,0 +1,116 @@
+const canvas = document.getElementById('my-canvas');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let particleArray = [];
+let adjustX = 5;
+let adjustY = 2;
+
+const mouse = {
+    x: null,
+    y: null,
+    radius: 150
+}
+
+window.addEventListener('mousemove', function(e){
+    mouse.x = e.x;
+    mouse.y = e.y;
+});
+
+ctx.fillStyle = 'white';
+ctx.font = `19px Verdana`;
+ctx.fillText('TAUSPACE', 0, 40);
+
+const textCoordinates = ctx.getImageData(0, 0, 100, 100);
+
+class Particle {
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+        this.size = 2;
+        this.baseX = this.x;
+        this.baseY = this.y;
+        this.density = ( Math.random() * 150 ) + 30;
+    }
+
+    draw(){
+        ctx.fillStyle = 'white';
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.closePath();
+        ctx.fill();
+
+    }
+    update(){
+        let dx = mouse.x - this.x;
+        let dy = mouse.y - this.y;
+        let distance = Math.sqrt(dx * dx + dy * dy);
+        let forceDirectionX = dx / distance;
+        let forceDirectionY = dy / distance;
+        let maxDistance = mouse.radius;
+        let force = (maxDistance - distance) / maxDistance;
+        let directionX = forceDirectionX * force * this.density;
+        let directionY = forceDirectionY * force * this.density
+        if(distance < mouse.radius){
+            this.x -= directionX;
+            this.y -= directionY;
+        }else {
+            if(this.x !== this.baseX){
+                let dx = this.x - this.baseX
+                this.x -= dx / 10;
+            }
+            if(this.y !== this.baseY){
+                let dy = this.y - this.baseY;
+                this.y -= dy / 10;
+            }
+        }
+    }
+}
+
+function init(){
+    particleArray = [];
+    for(let y = 0, y2 = textCoordinates.height; y < y2; y++){
+        for(let x = 0, x2 = textCoordinates.width; x < x2; x++){
+            if(textCoordinates.data[(y * 4 *textCoordinates.width) + (x * 4)] > 128){
+                let positionX = x + adjustX;
+                let positionY = y + adjustY;
+                particleArray.push(new Particle(positionX * 15, positionY * 15))
+            }
+        }
+    }
+}
+init();
+
+
+function animate(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    for (let i = 0; i < particleArray.length; i++) {
+       particleArray[i].draw();
+       particleArray[i].update();      
+    }
+    connect();
+    requestAnimationFrame(animate)
+}
+animate();
+
+function connect(){
+    let opacityValue = 1;
+    for(let a = 0; a < particleArray.length; a++){
+        for(let b = a; b < particleArray.length; b++){
+            let dx = particleArray[a].x - particleArray[b].x;
+            let dy = particleArray[a].y - particleArray[b].y;
+            let distance = Math.sqrt(dx * dx + dy * dy);
+            
+            opacityValue = 1 - (distance/20)
+                ctx.strokeStyle = 'rgb(255, 255, 255,' + opacityValue + ')';
+            if(distance < 20){               
+                ctx.lineWidth = 1;
+                ctx.beginPath();
+                ctx.moveTo(particleArray[a].x, particleArray[a].y);
+                ctx.lineTo(particleArray[b].x, particleArray[b].y);
+                ctx.stroke();
+            }
+        }
+    }
+}
